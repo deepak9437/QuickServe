b6179b1f8fdb03076577421e927f4684bc8acf73
Deepak Behera <162359292+deepak9437@users.noreply.github.com>
2026-05-16 10:27:20 +0530

    Backend file uploaded

---
+++ Backend/src/main/java/quick/serve/QuickServeApplication.java
@@ -0,0 +1,13 @@
+package quick.serve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickServeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickServeApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/quick/serve/QuickServeApplicationTests.java
@@ -0,0 +1,13 @@
+package quick.serve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickServeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
