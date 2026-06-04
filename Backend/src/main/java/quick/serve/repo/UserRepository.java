698d0b0603cc77a26b00959782ace088f92ff855
Deepak Behera <162359292+deepak9437@users.noreply.github.com>
2026-06-04 17:38:47 +0530

    latest backend

---
+++ Backend/src/main/java/quick/serve/repo/UserRepository.java
@@ -0,0 +1,15 @@
+package quick.serve.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import quick.serve.entity.UserEntity;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Integer>{
+
+	UserEntity findByUserEmailAndPassword(String userEmail, String password);
+
+//	UserEntity findByUserUserEmailAndUserPasswordAndStatus(String userEmail, String password ,String status);
+	
+}
