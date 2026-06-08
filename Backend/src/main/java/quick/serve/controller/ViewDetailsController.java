66e3e9f265b56611aabafbe39db1b7a9e66c97e9
Soumya-Ranjan35 <soumyaranjan27115@gmail.com>
2026-06-08 11:36:04 +0530

    new change

--- Backend/src/main/java/quick/serve/controller/ViewDetailsController.java
+++ Backend/src/main/java/quick/serve/controller/ViewDetailsController.java
@@ -3,6 +3,7 @@ package quick.serve.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import quick.serve.dto.ProviderDTO;
 import quick.serve.service.ViewService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/view")
 public class ViewDetailsController {
