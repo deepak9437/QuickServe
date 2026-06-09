4b510670b31af2e8dcbfdf0c88c73c013f82a2ea
deepak9437 <bapunbehera042@gmail.com>
2026-06-09 08:25:27 +0530

    costumer dashboard and profile

--- Frontend/src/app/features/customer/dashboard/dashboard.ts
+++ Frontend/src/app/features/customer/dashboard/dashboard.ts
@@ -1,9 +1,46 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+  styleUrl: './dashboard.css'
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+
+  user: any = {};
+
+  totalBookings = 0;
+  activeRequests = 0;
+  favouriteProviders = 0;
+
+  recentBookings = [
+    {
+      serviceName: 'Electrician',
+      status: 'Completed'
+    },
+    {
+      serviceName: 'Plumber',
+      status: 'Pending'
+    }
+  ];
+
+  constructor(private router: Router) {
+
+    this.user = JSON.parse(
+      localStorage.getItem('user') || '{}'
+    );
+  }
+
+  goToProfile() {
+    this.router.navigate(['/customer-profile']);
+  }
+
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['/login']);
+  }
+}
--- Frontend/src/app/features/customer/profile/profile.ts
+++ Frontend/src/app/features/customer/profile/profile.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-profile',
-  imports: [],
+  selector: 'app-customer-profile',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './profile.html',
-  styleUrl: './profile.css',
+  styleUrl: './profile.css'
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+
+  user: any = {};
+
+  constructor() {
+
+    this.user = JSON.parse(
+      localStorage.getItem('user') || '{}'
+    );
+  }
+
+  editProfile() {
+    alert('Navigate to Edit Profile Page');
+  }
+}
