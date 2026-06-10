ca5ea2f3d2b99600d66e061c85afa3824b3117dd
Soumya-Ranjan35 <soumyaranjan27115@gmail.com>
2026-06-10 11:39:24 +0530

    adminDash

--- Frontend/src/app/features/admin/dashboard/dashboard.ts
+++ Frontend/src/app/features/admin/dashboard/dashboard.ts
@@ -1,6 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { CommonModule } from "@angular/common";
+import { AdminService } from "../../../core/services/admin";
 
 @Component({
   selector: "app-admin-dashboard",
@@ -9,8 +10,46 @@ import { CommonModule } from "@angular/common";
   templateUrl: "./dashboard.html",
   styleUrl: "./dashboard.css",
 })
-export class AdminDashboardComponent {
-  constructor(private router: Router) {}
+export class AdminDashboardComponent implements OnInit {
+  loading = true;
+
+  dashboardStats = {
+    totalUsers: 0,
+    totalProviders: 0,
+    pendingApprovals: 0,
+    totalBookings: 0,
+  };
+
+  recentActivities: string[] = [];
+
+  constructor(
+    private router: Router,
+    private adminService: AdminService,
+  ) {}
+
+  ngOnInit(): void {
+    this.loadDashboardData();
+  }
+
+  loadDashboardData(): void {
+    this.adminService.getDashboardStats().subscribe({
+      next: (response: any) => {
+        this.dashboardStats = {
+          totalUsers: response.totalUsers,
+          totalProviders: response.totalProviders,
+          pendingApprovals: response.pendingApprovals,
+          totalBookings: response.totalBookings,
+        };
+
+        this.recentActivities = response.recentActivities || [];
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error("Dashboard load failed", err);
+        this.loading = false;
+      },
+    });
+  }
 
   goToUsers() {
     this.router.navigate(["/admin/users"]);
