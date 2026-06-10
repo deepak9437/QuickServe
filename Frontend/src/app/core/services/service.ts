049af37d231443b617bebb3800c1199b90fcc5f3
Soumya-Ranjan35 <soumyaranjan27115@gmail.com>
2026-06-10 08:09:16 +0530

    new api

--- Frontend/src/app/core/services/service.ts
+++ Frontend/src/app/core/services/service.ts
@@ -1,15 +1,15 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class Service {
-  private apiUrl = 'http://localhost:3030/quickserve';
+  private apiUrl = "http://localhost:3030/quickserve";
 
   constructor(private http: HttpClient) {}
 
   viewProviders() {
-    return this.http.get(`${this.apiUrl}/view/all`);
+    return this.http.get(`${this.apiUrl}/view/approved`);
   }
 }
