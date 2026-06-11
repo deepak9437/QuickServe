edbac26f7aa16ef7fd10b85d5f0804aded691b0d
Soumya-Ranjan35 <soumyaranjan27115@gmail.com>
2026-06-11 08:50:34 +0530

    icon tab

--- Frontend/src/app/core/services/provider.ts
+++ Frontend/src/app/core/services/provider.ts
@@ -1,16 +1,16 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class ProviderService {
-  private apiUrl = 'http://localhost:3030/quickserve/view';
+  private apiUrl = "http://localhost:3030/quickserve";
 
   constructor(private http: HttpClient) {}
 
   getAllProviders(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/all`);
+    return this.http.get<any[]>(`${this.apiUrl}/view/approved`);
   }
 }
