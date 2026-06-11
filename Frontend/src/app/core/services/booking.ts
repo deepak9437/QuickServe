a5b7f15b777ae192b540357ab97aaeaaaddeb822
Soumya-Ranjan35 <soumyaranjan27115@gmail.com>
2026-06-11 08:04:44 +0530

    navbar changes

--- Frontend/src/app/core/services/booking.ts
+++ Frontend/src/app/core/services/booking.ts
@@ -1,30 +1,26 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/internal/Observable';
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/internal/Observable";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class ServiceBooking {
+  private apiUrl = "http://localhost:3030/quickserve";
 
-  private apiUrl = 'http://localhost:3030/quickserve';
+  constructor(private http: HttpClient) {}
 
-   constructor(private http: HttpClient) {}
-
-    bookService(data: any): Observable<any> {
-
+  bookService(data: any): Observable<any> {
     let params = new HttpParams()
-      .set('uId', data.uId)
-      .set('pId', data.pId)
-      .set('serviceName', data.serviceName)
-      .set('problem', data.problem)
-      .set('address', data.address)
-      .set('bookingDate', data.bookingDate);
+      .set("uId", data.uId)
+      .set("pId", data.pId)
+      .set("serviceName", data.serviceName)
+      .set("problem", data.problem)
+      .set("address", data.address)
+      .set("bookingDate", data.bookingDate);
 
-    return this.http.post(
-      `${this.apiUrl}/booking/costumer_booking`,
-      null,
-      { params }
-    );
+    return this.http.post(`${this.apiUrl}/booking/customer_booking`, null, {
+      params,
+    });
   }
 }
