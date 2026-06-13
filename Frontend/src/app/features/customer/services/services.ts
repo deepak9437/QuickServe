e82cd3359fcc326eecea08d18f81d35377ed3e6f
Soumya-Ranjan35 <soumyaranjan27115@gmail.com>
2026-06-13 19:02:29 +0530

    service page

--- Frontend/src/app/features/customer/services/services.ts
+++ Frontend/src/app/features/customer/services/services.ts
@@ -1,30 +1,25 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { Router, RouterModule } from '@angular/router';
-import { Service } from '../../../core/services/service';
+import { CommonModule } from "@angular/common";
+import { ChangeDetectorRef, Component, OnInit } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { Router, RouterModule } from "@angular/router";
+import { Service } from "../../../core/services/service";
 
 @Component({
-  selector: 'app-services',
+  selector: "app-services",
   standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule,
-    RouterModule
-  ],
-  templateUrl: './services.html',
-  styleUrl: './services.css'
+  imports: [CommonModule, FormsModule, RouterModule],
+  templateUrl: "./services.html",
+  styleUrl: "./services.css",
 })
 export class ServicesComponent implements OnInit {
-
   constructor(
     private service: Service,
     private cdr: ChangeDetectorRef,
-    private router: Router
+    private router: Router,
   ) {}
 
-  searchText = '';
-  selectedCategory = '';
+  searchText = "";
+  selectedCategory = "";
 
   currentPage = 1;
   itemsPerPage = 3;
@@ -36,11 +31,8 @@ export class ServicesComponent implements OnInit {
   }
 
   viewProviders(): void {
-
     this.service.viewProviders().subscribe({
-
       next: (data: any) => {
-
         console.log(JSON.stringify(data, null, 2));
 
         this.providers = data;
@@ -49,67 +41,46 @@ export class ServicesComponent implements OnInit {
       },
 
       error: (error: any) => {
-
-        console.error('ERROR:', error);
-      }
+        console.error("ERROR:", error);
+      },
     });
   }
 
   get filteredProviders() {
-
-    return this.providers.filter(provider => {
-
-      const matchName =
-        provider.user?.fullName
-          ?.toLowerCase()
-          .includes(this.searchText.toLowerCase());
+    return this.providers.filter((provider) => {
+      const matchSkill = provider.skills
+        ?.toLowerCase()
+        .includes(this.searchText.toLowerCase());
 
       const matchCategory =
         !this.selectedCategory ||
-        provider.skills?.toLowerCase() ===
-        this.selectedCategory.toLowerCase();
+        provider.skills?.toLowerCase() === this.selectedCategory.toLowerCase();
 
-      return matchName && matchCategory;
+      return matchSkill && matchCategory;
     });
   }
 
   get paginatedProviders() {
+    const start = (this.currentPage - 1) * this.itemsPerPage;
 
-    const start =
-      (this.currentPage - 1) * this.itemsPerPage;
-
-    return this.filteredProviders.slice(
-      start,
-      start + this.itemsPerPage
-    );
+    return this.filteredProviders.slice(start, start + this.itemsPerPage);
   }
 
   get totalPages() {
-
-    return Array(
-      Math.ceil(
-        this.filteredProviders.length /
-        this.itemsPerPage
-      )
-    )
+    return Array(Math.ceil(this.filteredProviders.length / this.itemsPerPage))
       .fill(0)
       .map((_, i) => i + 1);
   }
 
   changePage(page: number): void {
-
     this.currentPage = page;
   }
 
   bookNow(provider: any): void {
+    console.log("Selected Provider:", provider);
 
-    console.log('Selected Provider:', provider);
+    sessionStorage.setItem("selectedProvider", JSON.stringify(provider));
 
-    sessionStorage.setItem(
-      'selectedProvider',
-      JSON.stringify(provider)
-    );
-
-    this.router.navigate(['/booking']);
+    this.router.navigate(["/booking"]);
   }
 }
